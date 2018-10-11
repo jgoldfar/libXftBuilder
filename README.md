08622b21007f3a5d9db885f17ddfb4deb00bf8b1
Jonathan Goldfarb <jgoldfar@gmail.com>
2018-10-11 12:50:24 -0400

    Add autogenerated files to build libXft

---
+++ README.md
@@ -0,0 +1,9 @@
+# libXftBuilder
+
+[![Build Status](https://travis-ci.org/jgoldfar/libXftBuilder.svg?branch=master)](https://travis-ci.org/jgoldfar/libXftBuilder)
+
+This repository builds binary artifacts for the libXft project. Binary artifacts are automatically uploaded to
+[this repository's GitHub releases page](https://github.com/jgoldfar/libXftBuilder/releases) whenever a tag is created
+on this repository.
+
+This repository was created using [BinaryBuilder.jl](https://github.com/JuliaPackaging/BinaryBuilder.jl)
---
+++ build_tarballs.jl
@@ -0,0 +1,42 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+# Collection of sources required to build libXft
+sources = [
+    "https://gitlab.freedesktop.org/xorg/lib/libxft.git" =>
+    "c418dc7594f98359ae10815f62ee3efc0a511cf8",
+
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/libxft
+apk update
+apk add freetype freetype-dev fontconfig fontconfig-dev libx11 libx11-dev libxrender libxrender-dev libxext util-macros
+sh autogen.sh --sysconfdir=/etc --prefix=$prefix --host=$target --mandir=/usr/share/man CFLAGS="-I/usr/include/ -I/usr/include/freetype2/" LDFLAGS="-L/usr/lib/"
+make
+make install
+
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [
+    Linux(:x86_64, :glibc),
+    Linux(:x86_64, :musl)
+]
+
+# The products that we will ensure are always built
+products(prefix) = [
+    LibraryProduct(prefix, "libXft", Symbol("libXft.so"))
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, "libXft", sources, script, platforms, products, dependencies)
+
